71d2c817a43adaebccc5644309efe8244661e27c
Chris Malmberg <chma00@gmail.com>
2019-01-27 14:35:38 +0000

    main.cpp

---
+++ src/main/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by chris on 27/01/19.
+//
+
+#include <iostream>
+#include "main.h"
+
+int main() {
+    std::cout << "Hello world" std::endl;
+    return 0;
+}
---
+++ src/main/main.h
@@ -0,0 +1,14 @@
+//
+// Created by chris on 27/01/19.
+//
+
+#ifndef TEST_MAIN_H
+#define TEST_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //TEST_MAIN_H
